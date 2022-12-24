c0efd2f3757a2e70b76f5177f212bae1193ecf2e
Visith Ilayperuma <visith.h.ilayperuma@gmail.com>
2022-12-24 22:44:38 +0530

    Responsive Update

--- src/js/index.js
+++ src/js/index.js
@@ -9,16 +9,17 @@ for (let i = 0; i < btns.length; i++) {
 }
 
 window.onscroll = function () {
-  myFunction();
+  sticky();
 };
 
 var navbar = document.getElementById("navigation");
 window.addEventListener("scroll", () => {
   navbar.classList.toggle("sticky", window.scrollY > 0);
+  navbar.classList.toggle("nonsticky", window.scrollY > 50);
 });
 var sticky = navbar.offsetTop;
 
-function myFunction() {
+function sticky() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
     console.log("sticky");
@@ -50,95 +51,4 @@ $(document).ready(function () {
   });
 });
 
-// // Scrolling Effect
-
-// $(window).on("scroll", function () {
-//   if ($(window).scrollTop()) {
-//     $("nav").addClass("black");
-//   } else {
-//     $("nav").removeClass("black");
-//   }
-// });
-
-// Scrolling Effect 
-
 ScrollReveal().reveal('.team');
-
-// Shaky Effect
-// (function() {
-//   // Init
-//   var leoimg = document.getElementById("leoimg"),
-//     inner = document.getElementById("inner");
-
-//   // Mouse
-//   var mouse = {
-//     _x: 0,
-//     _y: 0,
-//     x: 0,
-//     y: 0,
-//     updatePosition: function(event) {
-//       var e = event || window.event;
-//       this.x = e.clientX - this._x;
-//       this.y = (e.clientY - this._y) * -1;
-//     },
-//     setOrigin: function(e) {
-//       this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
-//       this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
-//     },
-//     show: function() {
-//       return "(" + this.x + ", " + this.y + ")";
-//     }
-//   };
-
-//   // Track the mouse position relative to the center of the leoimg.
-//   mouse.setOrigin(leoimg);
-
-//   //-----------------------------------------
-
-//   var counter = 0;
-//   var updateRate = 10;
-//   var isTimeToUpdate = function() {
-//     return counter++ % updateRate === 0;
-//   };
-
-//   //-----------------------------------------
-
-//   var onMouseEnterHandler = function(event) {
-//     update(event);
-//   };
-
-//   var onMouseLeaveHandler = function() {
-//     inner.style = "";
-//   };
-
-//   var onMouseMoveHandler = function(event) {
-//     if (isTimeToUpdate()) {
-//       update(event);
-//     }
-//   };
-
-//   //-----------------------------------------
-
-//   var update = function(event) {
-//     mouse.updatePosition(event);
-//     updateTransformStyle(
-//       (mouse.y / inner.offsetHeight / 2).toFixed(2),
-//       (mouse.x / inner.offsetWidth / 2).toFixed(2)
-//     );
-//   };
-
-//   var updateTransformStyle = function(x, y) {
-//     var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
-//     inner.style.transform = style;
-//     inner.style.webkitTransform = style;
-//     inner.style.mozTransform = style;
-//     inner.style.msTransform = style;
-//     inner.style.oTransform = style;
-//   };
-
-//   //-----------------------------------------
-
-//   leoimg.onmouseenter = onMouseEnterHandler;
-//   leoimg.onmouseleave = onMouseLeaveHandler;
-//   leoimg.onmousemove = onMouseMoveHandler;
-// })();
